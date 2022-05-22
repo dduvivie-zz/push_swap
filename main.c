d64e46d6bbc961f7010812d94f892127906c41cb
Fulou-Shotoku <dvvxdn329@gmail.com>
2022-05-23 00:07:28 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+
+
+/*
+check:
+1. the number of parameters
+2. all arguments must be integer
+3. nothing can be bigger than a integer
+4. no duplicates
+*/
+int main(int argc, char *argv[])
+{
+    
+
+}
