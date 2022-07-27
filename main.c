b3ed356875a510245c8e6d79f98b3628e6da1de1
Danae Duvivier <Danae.Duvivier@ulb.ac.be>
2022-07-27 02:46:03 +0200

    worked on all operations

--- main.c
+++ main.c
@@ -2,13 +2,42 @@
 
 int	main(void)
 {
-	int stack[] = {2, 1, 3, 6, 5, 8};
-	t_list	stack1;
+	int args[] = {1, 2, 3, 4, 5, 6};
+	t_stack	stack1;
+	t_stack	stack2;
 
-	stack1 = init_stack(stack, 6);
-	//ft_printf("%d\n", stack1.head->val);
-	//ft_printf("%d\n", stack1.tail->val);
-	//ft_printf("PASS\n");
+	stack1 = init_stack(args, 6);
+	stack2 = init_stack(NULL, 0);
+
+	ft_printf("a\n");
 	display_stack(&stack1);
+	ft_printf("head : %d, tail : %d\n", stack1.head->val, stack1.tail->val);
+
+	// swap
+	swap(&stack1);
+	ft_printf("a swap\n");
+	display_stack(&stack1);
+	ft_printf("head : %d, tail : %d\n", stack1.head->val, stack1.tail->val);
+
+	// push
+	push(&stack1, &stack2);
+	ft_printf("a push a\n");
+	display_stack(&stack1);
+	ft_printf("head : %d, tail : %d\n", stack1.head->val, stack1.tail->val);
+	display_stack(&stack2);
+	ft_printf("head : %d, tail : %d\n", stack2.head->val, stack2.tail->val);
+
+	// rotate
+	rotate(&stack1);
+	ft_printf("a rotate\n");
+	display_stack(&stack1);
+	ft_printf("head : %d, tail : %d\n", stack1.head->val, stack1.tail->val);
+
+	// reserve_rotate
+	reverse_rotate(&stack1);
+	ft_printf("a reserve_rotate\n");
+	display_stack(&stack1);
+	ft_printf("head : %d, tail : %d\n", stack1.head->val, stack1.tail->val);
+
 	return (0);
 }
---
+++ operations.c
@@ -0,0 +1,70 @@
+#include "push_swap.h"
+
+void	swap(t_stack *stack)
+{
+	t_node	*first_node;
+	t_node	*second_node;
+
+	if (stack->size > 1)
+	{
+		first_node = stack->head;
+		second_node = stack->head->next;
+		first_node->next = second_node->next;
+		stack->head = second_node;
+		stack->head->next = first_node;
+	}
+	if (stack->size == 2)
+	{
+		stack->tail = stack->head->next;
+	}
+}
+
+void	push(t_stack *stack1, t_stack *stack2)
+{
+	t_node	*head1;
+	t_node	*head2;
+
+	if (stack1->size < 1)
+		return ;
+	head1 = stack1->head;
+	head2 = stack2->head;
+	stack1->head = stack1->head->next;
+	stack2->head = head1;
+	stack2->head->next = head2;
+	stack1->size--;
+	stack2->size++;
+	if (stack2->tail == NULL)
+		stack2->tail = stack2->head;
+	if (stack1->size == 0)
+		stack1->tail = NULL;
+}
+
+void	rotate(t_stack *stack)
+{
+	t_node	*new_head;
+
+	if (stack->size < 2)
+		return ;
+	new_head = stack->head->next;
+	stack->head->next = NULL;
+	stack->tail->next = stack->head;
+	stack->tail = stack->tail->next;
+	stack->head = new_head;
+}
+
+void	reverse_rotate(t_stack *stack)
+{
+	t_node	*new_tail;
+	int		num;
+
+	if (stack->size < 2)
+		return ;
+	new_tail = stack->head;
+	num = 1;
+	while (++num < stack->size)
+		new_tail = new_tail->next;
+	stack->tail->next = stack->head;
+	stack->head = stack->tail;
+	stack->tail = new_tail;
+	stack->tail->next = NULL;
+}
