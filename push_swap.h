9caec063ae845207e98371a472d7b68162ee8380
Danae Duvivier <Danae.Duvivier@ulb.ac.be>
2022-07-27 02:44:30 +0200

    update header file

--- push_swap.h
+++ push_swap.h
@@ -24,12 +24,17 @@ typedef struct s_node{
 	t_node	*next;
 }				t_node;
 
-typedef struct s_list{
+typedef struct s_stack{
 	t_node	*head;
 	t_node	*tail;
-}				t_list;
+	int		size;
+}				t_stack;
 
-t_list	init_stack(int *stack, int len);
-void	display_stack(t_list *list);
+t_stack	init_stack(int *args, int len);
+void	display_stack(t_stack *stack);
+void	swap(t_stack *stack);
+void	push(t_stack *stack1, t_stack *stack2);
+void	rotate(t_stack *stack);
+void	reverse_rotate(t_stack *stack);
 
 #endif
