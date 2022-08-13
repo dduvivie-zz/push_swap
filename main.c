c83a811e8a99457a9b591e39e0e74f05a3ba992e
Danae Duvivier <Danae.Duvivier@ulb.ac.be>
2022-08-13 19:41:32 +0200

    need to remove system
    leaks check

--- main.c
+++ main.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dduvivie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/23 12:57:05 by dduvivie          #+#    #+#             */
+/*   Updated: 2022/08/30 19:50:06 by dduvivie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "push_swap.h"
 
 int	main(int argc, char *argv[])
@@ -19,8 +30,6 @@ int	main(int argc, char *argv[])
 	stack_a = init_stack(int_list, argc - 1);
 	stack_b = init_stack(NULL, 0);
 	free(int_list);
-	//display_stack(&stack_a);
-	//display_index(&stack_a);
 	start(&stack_a, &stack_b);
 	free_stack(&stack_a, 0);
 	free_stack(&stack_b, 0);
