edccd4bcbb3d283cc6a6627a61167306e5b58998
Danae Duvivier <Danae.Duvivier@ulb.ac.be>
2022-08-15 03:35:10 +0200

    DONE. Header file

--- push_swap.h
+++ push_swap.h
@@ -21,7 +21,7 @@ typedef struct s_node	t_node;
 // structure
 typedef struct s_node{
 	int		val;
-	int		index;
+	int		order;
 	t_node	*next;
 }				t_node;
 
@@ -33,24 +33,24 @@ typedef struct s_stack{
 
 int		args_are_correct(int argc, char *argv[], int *int_list);
 t_stack	init_stack(int *args, int len);
-void	set_node_index(t_stack *stack);
-
+void	set_node_order(t_stack *stack);
 void	display_stack(t_stack *stack);
-void	display_index(t_stack *stack);
+void	display_node_order(t_stack *stack);
+void	free_stack(t_stack *stack, int error_flag);
 int		stack_is_sort(t_node *node);
+void	start_sort(t_stack *stack_a, t_stack *stack_b);
 
+/* operations */
 void	swap(t_stack *stack);
 void	push(t_stack *stack1, t_stack *stack2);
 void	rotate(t_stack *stack);
 void	reverse_rotate(t_stack *stack);
-void	start(t_stack *stack_a, t_stack *stack_b);
 void	call_operation(int num, t_stack *stack_a, t_stack *stack_b);
 
+/* sort functions */
 void	radix_sort(t_stack *stack_a, t_stack *stack_b);
 void	sort_2_int(t_stack *stack_a, t_stack *stack_b);
 void	sort_3_int(t_stack *a, t_stack *b);
 void	sort_4_int(t_stack *a, t_stack *b);
 void	sort_5_int(t_stack *a, t_stack *b);
-void	free_stack(t_stack *stack, int error_flag);
-
 #endif
