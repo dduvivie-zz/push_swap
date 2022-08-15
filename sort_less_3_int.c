59227f33af38d0c565b394e92a887a1a5bc6aa73
Danae Duvivier <dduvivie@fu-c4-p2.s19.be>
2022-08-15 15:40:00 +0200

    DONE. Sort 3 or 2 integers

--- sort_less_3_int.c
+++ sort_less_3_int.c
@@ -1,11 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_less_3_int.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dduvivie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/01 15:38:37 by dduvivie          #+#    #+#             */
+/*   Updated: 2022/08/15 15:38:40 by dduvivie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "push_swap.h"
 
+/* Sort 2 integers. */
 void	sort_2_int(t_stack *stack_a, t_stack *stack_b)
 {
 	if (stack_a->head->val > stack_a->head->next->val)
 		call_operation(0, stack_a, stack_b);
 }
 
+/* Sort 3 integers. */
 void	sort_3_int(t_stack *a, t_stack *b)
 {
 	int	head;
