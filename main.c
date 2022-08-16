fcef5490b14c96a6323071f6b657b6541f33d9b7
Danae Duvivier <Danae.Duvivier@ulb.ac.be>
2022-08-16 17:04:28 +0200

    DONE. main

--- main.c
+++ main.c
@@ -6,28 +6,30 @@
 /*   By: dduvivie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 12:57:05 by dduvivie          #+#    #+#             */
-/*   Updated: 2022/08/30 19:50:06 by dduvivie         ###   ########.fr       */
+/*   Updated: 2022/08/15 19:50:06 by dduvivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
+
+int	error_exit(t_args *args)
+{
+	write(STDERR_FILENO, "Error\n", ft_strlen("Error\n"));
+	free_args(args);
+	exit(1);
+}
 
 int	main(int argc, char *argv[])
 {
 	t_stack	stack_a;
 	t_stack	stack_b;
-	int		*int_list;
+	t_args	*args;
 
 	if (argc < 2)
 		return (0);
-	int_list = get_int_list(argc, argv);
-	if (!int_list)
-	{
-		ft_printf("Error\n");
-		return (0);
-	}
-	stack_a = init_stack(int_list, argc - 1);
+	args = get_int_array(argc, argv);
+	stack_a = init_stack(args->array, args->size);
 	stack_b = init_stack(NULL, 0);
-	free(int_list);
+	free_args(args);
 	start_sort(&stack_a, &stack_b);
 	free_stack(&stack_a, 0);
 	free_stack(&stack_b, 0);
